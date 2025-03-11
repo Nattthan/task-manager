bab8ee59f7541868185fb4974a8e9eac3fc2ba9a
Naff <104893192+Nattthan@users.noreply.github.com>
2025-03-11 09:57:20 +0100

    Update Tailwind CSS configuration and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    content: ["./src/**/*.{js,ts,tsx}"], // Ajuste selon ton projet
+    theme: {
+      extend: {},
+    },
+    safelist: [
+      "min-h-screen",
+      "flex",
+      "flex-col",
+      "items-center",
+      "justify-center",
+      "p-4",
+      "bg-gray-100",
+      "text-2xl",
+      "font-bold",
+      "mb-4",
+      "space-x-2",
+      "border",
+      "p-2",
+      "rounded-lg",
+      "bg-blue-500",
+      "text-blue-200",
+      "px-4",
+      "py-2",
+      "mt-4",
+      "border-b",
+    ],
+    plugins: [],
+  };
+  
